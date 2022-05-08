3aa7c67e7bace0433b658b36d916e23e38e09773
Kate E Jackson <cs15lsp22acr@ieng6-201.ucsd.edu>
2022-05-07 17:24:56 -0700

    deleting a line

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -58,8 +58,6 @@ public class MarkdownParse {
 
         return toReturn;
     }
-    // Make change
-
 
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
