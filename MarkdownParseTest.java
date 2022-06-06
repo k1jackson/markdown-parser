a9f3cdfcc245fb7f79631a1dd9dc3d37841c5893
Kate E Jackson <cs15lsp22acr@ieng6-203.ucsd.edu>
2022-06-05 20:42:19 -0700

    Add Test Files

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,55 +1,3 @@
-<<<<<<< HEAD
-import static org.junit.Assert.*; // Imports JUnit's Assert class to access static methods
-import java.util.ArrayList;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-import org.junit.*; // Imports JUnit
-
-public class MarkdownParseTest { // Creates class MarkdownParseTest
-    @Test
-    public void testFile() throws IOException {
-        ArrayList<String> exp = new ArrayList<String>();
-        exp.add("https://something.com");
-        exp.add("some-thing.html");
-
-        String content = Files.readString(Path.of("test-file.md"));
-
-        assertEquals(exp, MarkdownParse.getLinks(content));
-    }
-    @Test
-    public void testFileImg() throws IOException {
-        ArrayList<String> exp = new ArrayList<String>();
-        exp.add("https://something.com");
-
-        String content = Files.readString(Path.of("test-file-img.md"));
-
-        assertEquals(exp, MarkdownParse.getLinks(content));
-    }
-    @Test
-    public void testFileParen() throws IOException {
-        ArrayList<String> exp = new ArrayList<String>();
-
-        String content = Files.readString(Path.of("test-file-paren.md"));
-
-        assertEquals(exp, MarkdownParse.getLinks(content));
-    }
-    @Test
-    public void testFileParenLink() throws IOException {
-        ArrayList<String> exp = new ArrayList<String>();
-        exp.add("https://www.example.com/(parenthesis)");
-        exp.add("https://www.example.com/(parenthesis)");
-        exp.add("https://www.example.com/(par(en)thesis)");
-
-        String content = Files.readString(Path.of("test-file-paren-link.md"));
-
-        assertEquals(exp, MarkdownParse.getLinks(content));
-    }
-    // Make change
-}
-
-=======
 import static org.junit.Assert.*; // Imports JUnit's Assert class to access static methods
 import java.util.ArrayList;
 import java.io.IOException;
@@ -136,4 +84,3 @@ public class MarkdownParseTest { // Creates class MarkdownParseTest
     
 }
 
->>>>>>> ca9185a5ad6904d027fded09fb5e53d0f2635612
